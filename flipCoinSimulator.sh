375a617cba62bb389222997a55a3024f2c3a4fd1
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-27 15:00:58 +0530

    Add: As a Simulator start with Flipping a Coin to Display Heads or Tails as winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,4 +1,4 @@
-echo "Welcome To Flip Coin Simulation Problem "
+echo "Welcome All To Flip Coin Simulation Problem "
 read -p "Enter a coin value like 1 or 0 value: " x
 if [ $x == $((RANDOM%2)) ]
 then
